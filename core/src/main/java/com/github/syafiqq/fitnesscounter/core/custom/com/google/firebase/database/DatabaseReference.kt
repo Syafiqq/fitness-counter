374438a3590fbc46db4643c97a5b80ffd0678d6b
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-08 20:51:19 +0700

    Add Custom Database Reference

---
+++ core/src/main/java/com/github/syafiqq/fitnesscounter/core/custom/com/google/firebase/database/DatabaseReference.kt
@@ -0,0 +1,20 @@
+package com.github.syafiqq.fitnesscounter.core.custom.com.google.firebase.database
+
+import timber.log.Timber
+import com.google.firebase.database.DatabaseError as BaseDatabaseError
+import com.google.firebase.database.DatabaseReference as BaseDatabaseReference
+
+/**
+ * This fitness-counter project created by :
+ * Name         : syafiq
+ * Date / Time  : 08 January 2018, 8:44 PM.
+ * Email        : id.muhammad.syafiq@gmail.com
+ * Github       : Syafiqq
+ */
+class DatabaseReference
+{
+    interface CompleteListener: BaseDatabaseReference.CompletionListener
+    {
+        override fun onComplete(error: BaseDatabaseError?, dbRef: BaseDatabaseReference?) = Timber.d("onComplete [${error} ${dbRef}]")
+    }
+}
