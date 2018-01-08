1b935ebdebe1e08072539f1fb1525419735b75a3
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-08 20:35:27 +0700

    Add Path

---
+++ core/src/main/java/com/github/syafiqq/fitnesscounter/core/db/Path.kt
@@ -0,0 +1,15 @@
+package com.github.syafiqq.fitnesscounter.core.db
+
+/**
+ * This fitness-counter project created by :
+ * Name         : syafiq
+ * Date / Time  : 07 January 2018, 1:29 PM.
+ * Email        : id.muhammad.syafiq@gmail.com
+ * Github       : Syafiqq
+ */
+object Path
+{
+    val USERS = "users";
+    val GROUPS = "groups";
+    val USERS_GROUPS = "users_groups";
+}
