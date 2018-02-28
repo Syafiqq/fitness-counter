323765e04f990b537a7a0ae02a17ae8b43dab4fe
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-01 06:28:40 +0700

    Add Event Helper

---
+++ core/src/main/java/com/github/syafiqq/fitnesscounter/core/helpers/tester/EventHelper.kt
@@ -0,0 +1,10 @@
+package com.github.syafiqq.fitnesscounter.core.helpers.tester
+
+/**
+ * This fitness-counter project created by :
+ * Name         : syafiq
+ * Date / Time  : 01 March 2018, 6:27 AM.
+ * Email        : id.muhammad.syafiq@gmail.com
+ * Github       : Syafiqq
+ */
+object EventHelper
