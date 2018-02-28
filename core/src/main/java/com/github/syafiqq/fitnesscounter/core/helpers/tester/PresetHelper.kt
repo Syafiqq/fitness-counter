086014fc4046fd83e1a6d447fed0298735769802
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-01 06:36:06 +0700

    Add save medical checkup

--- core/src/main/java/com/github/syafiqq/fitnesscounter/core/helpers/tester/PresetHelper.kt
+++ core/src/main/java/com/github/syafiqq/fitnesscounter/core/helpers/tester/PresetHelper.kt
@@ -1,5 +1,12 @@
 package com.github.syafiqq.fitnesscounter.core.helpers.tester
 
+import com.github.syafiqq.fitnesscounter.core.custom.com.google.firebase.database.DatabaseReference
+import com.github.syafiqq.fitnesscounter.core.db.external.DataMapper
+import com.github.syafiqq.fitnesscounter.core.db.external.poko.tester.MedicalCheckup
+import com.google.firebase.database.FirebaseDatabase
+import timber.log.Timber
+import com.google.firebase.database.DatabaseReference as BaseDatabaseReference
+
 /**
  * This fitness-counter project created by :
  * Name         : syafiq
@@ -9,3 +16,29 @@ package com.github.syafiqq.fitnesscounter.core.helpers.tester
  */
 
 object PresetHelper
+{
+    //@formatter:off
+    private fun saveBranch(preset: String, queue: Int, branch:String, data:Any, callback: BaseDatabaseReference.CompletionListener = object :DatabaseReference.CompletionListener{})
+    { //@formatter:on
+        Timber.d("saveBranch [$preset, $queue, $branch, $data, $callback]")
+        val dbRef = FirebaseDatabase.getInstance().getReference("/")
+        val query = HashMap<String, Any>()
+        for ((key, value) in DataMapper.presetTester(preset, queue, branch))
+        {
+            when (key)
+            { // @formatter:off
+                "presets" -> {query[value] = data}
+            }// @formatter:on
+        }
+
+        dbRef.updateChildren(query, callback)
+
+    }
+
+    //@formatter:off
+    fun saveMedicalCheckUp(preset:String, queue:Int, data:MedicalCheckup, callback: BaseDatabaseReference.CompletionListener = object :DatabaseReference.CompletionListener{})
+    { //@formatter:on
+        Timber.d("saveMedicalCheckUp [$preset, $queue, $data, $callback]")
+        saveBranch(preset, queue, "medical", data, callback)
+    }
+}
