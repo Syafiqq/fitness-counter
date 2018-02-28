8147c7b7a157a4929d7f04fcf5e2e0293501f72a
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-01 06:29:14 +0700

    Add Preset Helper

---
+++ core/src/main/java/com/github/syafiqq/fitnesscounter/core/helpers/tester/PresetHelper.kt
@@ -0,0 +1,11 @@
+package com.github.syafiqq.fitnesscounter.core.helpers.tester
+
+/**
+ * This fitness-counter project created by :
+ * Name         : syafiq
+ * Date / Time  : 01 March 2018, 6:28 AM.
+ * Email        : id.muhammad.syafiq@gmail.com
+ * Github       : Syafiqq
+ */
+
+object PresetHelper
