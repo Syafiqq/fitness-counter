e7be78f9f18ec788e2f0fdb4ec0ca495e4024c7d
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-03 19:59:08 +0700

    Add Illinois Data Type

---
+++ core/src/main/java/com/github/syafiqq/fitnesscounter/core/db/external/poko/tester/Illinois.kt
@@ -0,0 +1,27 @@
+package com.github.syafiqq.fitnesscounter.core.db.external.poko.tester
+
+import com.google.firebase.database.PropertyName
+import java.io.Serializable
+
+/**
+ * This fitness-counter project created by :
+ * Name         : syafiq
+ * Date / Time  : 03 March 2018, 7:39 PM.
+ * Email        : id.muhammad.syafiq@gmail.com
+ * Github       : Syafiqq
+ */
+data class Illinois(
+        // @formatter:off
+        @get:PropertyName("start")        @set:PropertyName("start")        var start: Int? = null,
+        @get:PropertyName("end")          @set:PropertyName("end")          var end: Int? = null,
+        @get:PropertyName("elapsed")      @set:PropertyName("elapsed")      var elapsed: Int? = null
+        // @formatter:on
+): Serializable
+{
+    fun set(illinois: Illinois)
+    {
+        this.start = illinois.start
+        this.end = illinois.end
+        this.elapsed = illinois.elapsed
+    }
+}
